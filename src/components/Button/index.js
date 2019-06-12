6a0eb3dfe6225f26e3c43d93c5c81935623c8f41
Henrique Weiand <henrique.weiand@terceiroslinx.com.br>
2019-06-12 15:05:13 -0300

    Adicionado editorconfig e eslint conforme novas aulas

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends Component {
-  render() {
-    return (
-      <a onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
-  }
-}
+const Button = ({ onClick, children }) => <a onClick={onClick}>{children}</a>;
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.element
-}
+  children: PropTypes.element,
+};
 
 Button.defaultProps = {
-  children: <p>Sem valor</p>
-}
+  children: <p>Sem valor</p>,
+};
 
 export default Button;
